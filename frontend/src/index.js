0996d637a10f74f74ef18b5729046f2fa6c0cf55
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-01-14 18:09:55 +0530

    Redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./index.css";
 import "./bootstrap.min.css";
 
 render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
